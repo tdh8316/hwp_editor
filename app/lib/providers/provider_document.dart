bfba5e7b12515d385c53fefe053acc6ada5c11d4
탁동혁 <tdh8316@naver.com>
2022-05-12 00:09:55 +0900

    Add logic for deleting texts

--- app/lib/providers/provider_document.dart
+++ app/lib/providers/provider_document.dart
@@ -54,6 +54,7 @@ class DocumentPageProvider extends ChangeNotifier {
     final List<List<int>> _charShapes = (_paragraph["charShapes"] as List)
         .map((_tuple) => (_tuple as List).map((e) => e as int).toList())
         .toList();
+    // print("origin:${_charShapes}");
 
     // 텍스트가 추가된 것이라면
     if (text.length > prevText.length) {
@@ -62,6 +63,7 @@ class DocumentPageProvider extends ChangeNotifier {
       for (List _pair in IterableZip([text.characters, prevText.characters])) {
         if (_pair[0] != _pair[1]) _diff = _pair[0];
       }
+
       final int _currentCharShapeIndex = controller.getCurrentCharShapeIndex(
           // 만약 한국어 이외의 문자가 추가된 것이라면 - 1
           adjust: RegExp(r"^[ㄱ-ㅎㅏ-ㅣ가-힣]+$").hasMatch(_diff) ? 0 : 1);
@@ -72,14 +74,39 @@ class DocumentPageProvider extends ChangeNotifier {
           charShape[0] += _diff.length;
         }
       }
-    } else {
-      // TODO: Remove text
+    } else if (text.length < prevText.length) {
+      // 제거된 문자열?
+      String _diff = "";
+      for (List _pair in IterableZip([text.characters, prevText.characters])) {
+        if (_pair[0] != _pair[1]) _diff = _pair[1];
+      }
+
+      final int _currentCharShapeIndex = controller.getCurrentCharShapeIndex();
+
+      // 앞의 두 charShapes 의 지워진 후 position 이 동일하다면
+      if (_charShapes.length > _currentCharShapeIndex + 2 &&
+          _charShapes[_currentCharShapeIndex + 1][0] ==
+              _charShapes[_currentCharShapeIndex + 2][0] - 1) {
+        // 앞의 charShape 제거
+        _charShapes.removeAt(_currentCharShapeIndex + 1);
+      }
+      // 현재 charShape 인덱스 뒤로 계속 charShapes 가 있으면
+      if (_charShapes.length > _currentCharShapeIndex + 2) {
+        // 현재 charShape 뒤의 charShapes position -= _diff.length
+        for (int i = 1; _charShapes.length > i + _currentCharShapeIndex; i++) {
+          _charShapes[i + _currentCharShapeIndex][0] -= _diff.length;
+        }
+      } else {
+        if (text.length <= _charShapes.last.first) {
+          _charShapes.removeLast();
+        }
+      }
     }
     _paragraph["text"] = text;
     _paragraph["charShapes"] = _charShapes;
 
     controller.charShapes = _charShapes;
-
+    // print("result:${_charShapes}");
     notifyListeners();
   }
 }
