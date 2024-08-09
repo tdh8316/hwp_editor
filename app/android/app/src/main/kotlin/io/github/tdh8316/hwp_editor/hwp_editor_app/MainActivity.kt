e2e1ec853a20220c78bf07a945e251dea04ad43b
Donghyeok Tak <tdh8316@naver.com>
2024-08-09 14:25:25 +0900

    Delete app directory

--- app/android/app/src/main/kotlin/io/github/tdh8316/hwp_editor/hwp_editor_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package io.github.tdh8316.hwp_editor.hwp_editor_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- app/lib/models/model_fonts.dart
+++
@@ -1,4 +0,0 @@
-List<String> getFontNames() => [
-      "함초롬바탕",
-      "맑은 고딕",
-    ];
